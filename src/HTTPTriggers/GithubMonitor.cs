5802a89bae7d7192dcf8b6d640d212132c24bc08
Raju Melveetilpurayil <melveer@sunmed.global>
2022-10-01 01:08:57 +0100

    [+] Code Initial commit

---
+++ src/HTTPTriggers/GithubMonitor.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using HTTPTriggers.Helper;
+
+namespace HTTPTriggers
+{
+    public static class GithubMonitor
+    {
+        [FunctionName("GithubMonitor")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            var result=await SendGridHelper.Execute();
+
+            return result ? new OkResult() : new NoContentResult();
+        }
+    }
+}
---
+++ src/HTTPTriggers/Helper/SendGridHelper.cs
@@ -0,0 +1,34 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace HTTPTriggers.Helper
+{
+    public class SendGridHelper
+    {
+        public static async Task<bool> Execute()
+        {
+
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var emailAddess = Environment.GetEnvironmentVariable("SENDGRID_EMAIL_ADDRESS");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(emailAddess, "Example User");
+            var subject = "Sending with SendGrid is Fun";
+            var to = new EmailAddress(emailAddess, "Example User");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
